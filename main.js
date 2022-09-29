015ce16e5e98600ba538bf3f5a78636b3e26860f
lolashot <ololadeshot@gmail.com>
2022-09-29 12:36:24 +0100

    making corrections

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 class allBooks {
-    constructor(id, title, author) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
-    }
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
     
-    static getBooks() {
-      return JSON.parse(localStorage.getItem('books'))
-        ? JSON.parse(localStorage.getItem('books'))
-        : [];
-    }
+  static getBooks() {
+    return JSON.parse(localStorage.getItem('books'))
+    ? JSON.parse(localStorage.getItem('books'))
+    : [];
+  }
   
-    static getBook() {
-      const books = allBooks.getBooks();
-      const booksCount = books.length;
-      const lastBook = booksCount - 1;
-      const AddedBook = books[lastBook]?.id
-        ? books[lastBook].id
-        : 0;
+  static getBook() {
+    const books = allBooks.getBooks();
+    const booksCount = books.length;
+    const lastBook = booksCount - 1;
+    const AddedBook = books[lastBook]?.id
+    ? books[lastBook].id
+    : 0;
   
-      return AddedBook + 1;
-    }
+    return AddedBook + 1;
+  }
   
-    static displayBooks() {
-      const booksStore = document.querySelector('.books-list');
-      const bookData = allBooks.getBooks();
-      if (bookData.length) {
-        const listOfBooks = bookData
-          .map(
-            (book) => `<div id="book_id" class="book">
-              <div class="title-and-author">
-              <div class="book-title">${book.title} by</div>
-              <div class="book-author">${book.author}</div>
-              </div>
-            <br />
-            <div class="remove_book-btn__container">
-              <button onclick="allBooks.removeBook(${book.id});" class="remove_book-btn">Remove</button>
-            </div>
-          </div><hr>`,
-          )
-          .join('');
-        booksStore.innerHTML = listOfBooks;
-      } else {
-        booksStore.innerHTML = ' <span class="text-center"><i> Please Add a Book</i></span>';
-      }
+  static displayBooks() {
+    const booksStore = document.querySelector('.books-list');
+    const bookData = allBooks.getBooks();
+    if (bookData.length) {
+      const listOfBooks = bookData
+      .map(
+      (book) => `<div id="book_id" class="book">
+        <div class="title-and-author">
+        <div class="book-title">${book.title} by</div>
+        <div class="book-author">${book.author}</div>
+        </div>
+        <br />
+        <div class="remove_book-btn__container">
+        <button onclick="allBooks.removeBook(${book.id});" class="remove_book-btn">Remove</button>
+        </div>
+        </div><hr>`,
+        )
+        .join('');
+      booksStore.innerHTML = listOfBooks;
+    } else {
+      booksStore.innerHTML = ' <span class="text-center"><i> Please Add a Book</i></span>';
     }
+  }
   
     static localStorage(a, b) {
       localStorage.setItem(a, JSON.stringify(b));
