91b22f5ab50b088d80bde35ebe9022952fd44b1e
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-29 21:38:29 +0600

    Fix linter errors

--- main.js
+++ main.js
@@ -84,31 +84,35 @@ document.getElementById('add-book-form').addEventListener('submit', (e) => {
 
 const navItems = document.getElementsByClassName('nav-item');
 for (let i = 0; i < navItems.length; i += 1) {
-  navItems[i].addEventListener('click', function () {
+  navItems[i].addEventListener('click', function navElements() {
     const current = document.getElementsByClassName('active');
     current[0].className = current[0]?.className?.replace(' active', '');
     this.className += ' active';
   });
 }
 
-function toggleSection(domId) {
-  if (domId === 'books-list') {
-    document.getElementById('add-books').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('books-list').style.display = 'block';
-  }
-
-  if (domId === 'add-books') {
-    document.getElementById('books-list').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('add-books').style.display = 'flex';
-  }
-
-  if (domId === 'contact') {
-    document.getElementById('books-list').style.display = 'none';
-    document.getElementById('add-books').style.display = 'none';
-    document.getElementById('contact').style.display = 'block';
-  }
+function listSelected() {
+  document.getElementById('add-books').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+  document.getElementById('books-list').style.display = 'block';
 }
 
+function addBooksSelected() {
+  document.getElementById('books-list').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+  document.getElementById('add-books').style.display = 'flex';
+}
 
+function contactSelected() {
+  document.getElementById('books-list').style.display = 'none';
+  document.getElementById('add-books').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+}
+
+const navList = document.querySelector('.list-nav');
+const navAdd = document.querySelector('.add-book-nav');
+const navContact = document.querySelector('.contact-nav');
+
+navList.addEventListener('click', listSelected);
+navAdd.addEventListener('click', addBooksSelected);
+navContact.addEventListener('click', contactSelected);
